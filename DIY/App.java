052129c72647a4c6b4ee0380bac731fda669fc8a
jinqiusky <108732806+jinqiusky@users.noreply.github.com>
2023-01-27 23:03:39 +0800

    Add files via upload

--- DIY/App.java
+++ DIY/App.java
@@ -1,6 +1,5 @@
 package com.github.tvbox.osc.base;
 
-import android.app.Activity;
 import androidx.multidex.MultiDexApplication;
 
 import com.github.tvbox.osc.bean.VodInfo;
@@ -8,9 +7,7 @@ import com.github.tvbox.osc.callback.EmptyCallback;
 import com.github.tvbox.osc.callback.LoadingCallback;
 import com.github.tvbox.osc.data.AppDataManager;
 import com.github.tvbox.osc.server.ControlManager;
-import com.github.tvbox.osc.util.AppManager;
-import com.github.tvbox.osc.util.EpgUtil;
-import com.github.tvbox.osc.util.FileUtils;
+import com.github.tvbox.osc.util.EpgNameFuzzyMatch;
 import com.github.tvbox.osc.util.HawkConfig;
 import com.github.tvbox.osc.util.OkGoHelper;
 import com.github.tvbox.osc.util.PlayerHelper;
@@ -18,6 +15,7 @@ import com.github.tvbox.osc.util.js.JSEngine;
 import com.kingja.loadsir.core.LoadSir;
 import com.orhanobut.hawk.Hawk;
 
+import me.jessyan.autosize.AutoSize;
 import me.jessyan.autosize.AutoSizeConfig;
 import me.jessyan.autosize.unit.Subunits;
 
@@ -35,8 +33,8 @@ public class App extends MultiDexApplication {
         instance = this;
         initParams();
         // OKGo
-        OkGoHelper.init(); //台标获取
-        EpgUtil.init();
+        OkGoHelper.init();
+        EpgNameFuzzyMatch.init();
         // 初始化Web服务器
         ControlManager.init(this);
         //初始化数据库
@@ -51,16 +49,18 @@ public class App extends MultiDexApplication {
                 .setSupportSubunits(Subunits.MM);
         PlayerHelper.init();
         JSEngine.getInstance().create();
-        FileUtils.cleanPlayerCache();
     }
 
     private void initParams() {
         // Hawk
         Hawk.init(this).build();
         Hawk.put(HawkConfig.DEBUG_OPEN, false);
-        putDefault(HawkConfig.HOME_REC, 0);       // Home Rec 0=豆瓣, 1=站点推荐, 2=历史
-        putDefault(HawkConfig.PLAY_TYPE, 1);      // Player   0=系统, 1=IJK, 2=Exo
-        putDefault(HawkConfig.IJK_CODEC, "硬解码");// IJK Render 软解码, 硬解码
+		Hawk.put(HawkConfig.HOME_REC, 1);       // Home Rec 0=豆瓣, 1=站点推荐, 2=历史
+		Hawk.put(HawkConfig.SEARCH_VIEW, 1);    // 0 文字搜索列表 1 缩略图搜索列表
+        if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
+            Hawk.put(HawkConfig.PLAY_TYPE, 1);
+			
+        }
     }
 
     public static App getInstance() {
@@ -81,13 +81,4 @@ public class App extends MultiDexApplication {
     public VodInfo getVodInfo(){
         return this.vodInfo;
     }
-
-    public Activity getCurrentActivity() {
-        return AppManager.getInstance().currentActivity();
-    }
-    private void putDefault(String key, Object value) {
-        if (!Hawk.contains(key)) {
-            Hawk.put(key, value);
-        }
-    }    
 }
